ff3aacd84b9d58fe53e07f13b1ca8d385c56a6e9
Frank Kelly <frankintech00@gmail.com>
2023-01-31 15:36:06 +0000

    successfully fetched movie data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
+import MovieCard from "./MovieCard";
 
+// This is the main component for our movie application.
 function App() {
+  // URL of the movie database API
   const API_URL = "https://api.themoviedb.org/3";
+  // API Key for accessing the database
   const API_KEY = "7312f41c50231306aa4e719fc961450a";
 
+  // State variable to store the movie data
+  const [movies, setMovies] = useState([]);
+
+  // Async function to fetch movie data from the API
   const fetchMovies = async () => {
-    const data = await axios.get(`${API_URL}/discover/movie`, {
+    // Get the data using axios and store the 'results' in a constant
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/discover/movie`, {
       params: {
-        api_key: API_KEY,
+        api_key: API_KEY, // Pass the API Key as a parameter
       },
     });
-    console.log("data", data);
+
+    // Set the state with the movie data
+    setMovies(results);
   };
 
+  // Use effect hook to run the `fetchMovies` function only once on component mount
   useEffect(() => {
     fetchMovies();
   }, []);
 
-  return <div></div>;
+  // Render the movie cards for each movie in the state
+  const renderMovies = () => {
+    return movies.map((movie) => {
+      // Return a MovieCard component for each movie, passing the movie data as a prop
+      return <MovieCard key={movie.id} movie={movie} />;
+    });
+  };
+
+  // Return the movie cards inside a div
+  return (
+    <div className="">
+      <div>{renderMovies()}</div>
+    </div>
+  );
 }
 
+// Export the App component as the default export
 export default App;
