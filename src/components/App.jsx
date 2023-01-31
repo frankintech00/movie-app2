a493a482b34adb8e814d637bf55e20e9a7a72757
Frank Kelly <frankintech00@gmail.com>
2023-01-31 14:56:34 +0000

    fetched movie data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 function App() {
-  return <div>Hello World!</div>;
+  const API_URL = "https://api.themoviedb.org/3";
+  const API_KEY = "7312f41c50231306aa4e719fc961450a";
+
+  const fetchMovies = async () => {
+    const data = await axios.get(`${API_URL}/discover/movie`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    console.log("data", data);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
